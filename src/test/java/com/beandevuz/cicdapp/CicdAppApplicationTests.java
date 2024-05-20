633dbf49c906b7fe04e2adf841df7e7daaef6b5a
= <=>
2024-05-20 21:45:42 +0700

    init commit

---
+++ src/test/java/com/beandevuz/cicdapp/CicdAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beandevuz.cicdapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
